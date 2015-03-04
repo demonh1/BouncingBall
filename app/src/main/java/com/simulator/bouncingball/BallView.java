05a2e8c9c8981b3b56a25c021a75fad867deaa1a
demonh1 <demon7987@gmail.com>
2015-03-04 20:21:15 +0300

    Update BallView.java

--- app/src/main/java/com/simulator/bouncingball/BallView.java
+++ app/src/main/java/com/simulator/bouncingball/BallView.java
@@ -25,9 +25,7 @@ public class BallView extends View implements Commons {
     //private StatusMessage ms;
 
     private float acceleration;
-
     private AudioPlayer collision;
-
 
     public BallView(Context context,int color, int backgroundColor, float acceleration) {
         super(context);
@@ -37,7 +35,6 @@ public class BallView extends View implements Commons {
 
         //ball = new Ball(Color.GREEN);
         //ms = new StatusMessage(Color.BLACK);
-
     }
 
     @Override
@@ -65,27 +62,22 @@ public class BallView extends View implements Commons {
             ball.setySpeed(-COEFFICIENT_OF_RESTITUTION * ball.getySpeed());
 
             collision.play();
-
         }
         else if (ball.getY() < 0) {
             ball.setY(ii);
             ball.setySpeed(-COEFFICIENT_OF_RESTITUTION * ball.getySpeed());
-
             collision.play();
         }
-
 
         // Ball is out of bounds in x dimension
         if (ball.getX() > xMax) {
             ball.setX(xMax-ii);
             ball.setxSpeed(-COEFFICIENT_OF_RESTITUTION * ball.getxSpeed());
-
             collision.play();
         }
         else if (ball.getX() < xMin) {
             ball.setX(xMin+ii);
             ball.setxSpeed(-COEFFICIENT_OF_RESTITUTION * ball.getxSpeed());
-
             collision.play();
         }
 
@@ -110,7 +102,6 @@ public class BallView extends View implements Commons {
                 ball = addBall(currentX, currentY, color);
             Log.d(TAG, "Action was DOWN");
         }
-
         return super.onTouchEvent(event);
     }
 
@@ -126,7 +117,6 @@ public class BallView extends View implements Commons {
         if(ball == null) { canvas.drawColor(backgroundColor); } /////
         else {
             //ms.draw(canvas);
-
             if(ball.getxSpeed() > TERM_SPEED) {
                 ball.draw(canvas);
 
@@ -134,10 +124,8 @@ public class BallView extends View implements Commons {
 
                     @Override
                     public void run() {
-
                         move();
                         //ms.sendInfo(ball);
-
                         try {
                             Thread.sleep(30);
                         } catch (InterruptedException e) { e.printStackTrace(); }
@@ -146,19 +134,15 @@ public class BallView extends View implements Commons {
 
                 });
                 thread.start();
-
                 // ms.sendInfo(ball);
             }
             //ms.sendInfo(ball);
         }
-
         invalidate();
-
     }
-
     public void setAudioPlayer(AudioPlayer audioPlayer) {
         collision = audioPlayer;
     }
 
-}
 
+}
