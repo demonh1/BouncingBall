8a7f5691d4685f786af3582051647d02e9d685e6
demonh1 <demon7987@gmail.com>
2015-03-13 01:27:56 +0300

    Update MainActivity.java

--- app/src/main/java/com/simulator/bouncingball/MainActivity.java
+++ app/src/main/java/com/simulator/bouncingball/MainActivity.java
@@ -14,7 +14,6 @@ public class MainActivity extends Activity {
 
     private static final String TAG = "Main Activity";
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,14 +23,10 @@ public class MainActivity extends Activity {
     public void onSelectStart(View view){
         Log.v(TAG,"onSelectStart");
         startActivity(new Intent(this,BallActivity.class));
-
     }
     public void onSelectSettings(View view){
         Log.v(TAG,"onSelectSettings");
         startActivity(new Intent(this,SettingsActivity.class));
-
-
     }
-
 
 }
